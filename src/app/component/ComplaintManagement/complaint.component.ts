745ca58b50f2022351f254d5018eff16ded0c163
ghofrane1111 <ghofrane.belhadef@esprit.tn>
2025-04-10 18:00:57 +0200

    initial commit: ajout de la template Angular

---
+++ src/app/component/ComplaintManagement/complaint.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'complaint.component.html',
+  standalone: true,
+})
+export class CardsComponent {}
---
+++ src/app/component/component.routing.ts
@@ -0,0 +1,51 @@
+import { Routes } from '@angular/router';
+import { NgbdpaginationBasicComponent } from './pagination/pagination.component';
+import { RiskManagementComponent } from './RiskManagement/RiskManagement.component';
+
+import { NgbdDropdownBasicComponent } from './DonationManagement/donation.component';
+import { NgbdnavBasicComponent } from './nav/nav.component';
+import { BadgeComponent } from './Loan Management/LoanManagement.component';
+import { NgbdButtonsComponent } from './ContractManagement/ContractManagement.component';
+import { CardsComponent } from './ComplaintManagement/complaint.component';
+import { TableComponent } from './table/table.component';
+
+
+export const ComponentsRoutes: Routes = [
+	{
+		path: '',
+		children: [
+			{
+				path: 'table',
+				component: TableComponent
+			},
+			{
+				path: 'card',
+				component: CardsComponent
+			},
+			{
+				path: 'pagination',
+				component: NgbdpaginationBasicComponent
+			},
+			{
+				path: 'badges',
+				component: BadgeComponent
+			},
+			{
+				path: 'alert',
+				component: RiskManagementComponent
+			},
+			{
+				path: 'dropdown',
+				component: NgbdDropdownBasicComponent
+			},
+			{
+				path: 'nav',
+				component: NgbdnavBasicComponent
+			},
+			{
+				path: 'buttons',
+				component: NgbdButtonsComponent
+			}
+		]
+	}
+];
